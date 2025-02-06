04e8faeeed58abc9c9f67d9cd73c68ac9a0c3cce
sunil sharma <gitbysuniel@gmail.com>
2025-02-06 19:41:55 +0900

    :: fixex logs

--- routes/newRoutes/auditLogRoute.js
+++ routes/newRoutes/auditLogRoute.js
@@ -1,22 +1,5 @@
 
 
-
-// // routes/auditLogRoutes.js
-// const express = require('express');
-// const controller = require ('../../controllers/auditLogController');
-// const router = express.Router();
-
-// router.get('/get-audit-log', controller.getLogs);
-// router.get('/exports-logs', controller.exportLogs);
-// router.delete('/clear-all-logs', controller.clearAllLogs);
-// router.delete('/clear-old-logs', controller.clearOldLogs);
-
-// module.exports = router;
-
-
-
-
-
 const express = require('express');
 const controller = require('../../controllers/auditLogController');
 const {requireLogin} = require ('../../middleware/newMiddleware/authMiddleware')
