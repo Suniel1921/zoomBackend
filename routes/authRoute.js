0dfb897a596e73bdf5d99aeb39b922fe9b2ed0ac
sunil sharma <gitbysuniel@gmail.com>
2025-03-07 15:09:55 +0900

    ::add rate limit route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -9,8 +9,8 @@ const { loginRateLimiter } = require("../middleware/newMiddleware/loginRateLimit
 
 
 router.post('/register', controller.register);
-// router.post('/login', loginRateLimiter, controller.login);
-router.post('/login', controller.login);
+router.post('/login', loginRateLimiter, controller.login);
+// router.post('/login', controller.login); without rate limit route
 router.get('/getLoggedInUser', requireLogin, controller.loggedIndUserData);
 router.get('/protectedRoute', requireLogin, controller.protectedRoute);
 router.get('/admin', requireLogin, isAdmin, controller.admin);
