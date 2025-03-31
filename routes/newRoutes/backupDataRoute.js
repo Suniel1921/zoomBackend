48b30a628c5c574a49c38b62161d05fb00da1c21
sunil sharma <gitbysuniel@gmail.com>
2025-03-31 19:38:05 +0900

    :: changed route name backup to backup-data

--- routes/newRoutes/backupDataRoute.js
+++ routes/newRoutes/backupDataRoute.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/backupDataController');
 
-router.get('/backup', controller.handleManualBackup);
+router.get('/backup-data', controller.handleManualBackup);
 router.get('/latest-backup', controller.getLatestBackup);
 router.post('/restore', controller.restoreBackup);
 router.post('/schedule', controller.scheduleBackup);
 router.post('/stop-schedule', controller.stopScheduledBackup);
-router.get('/schedule-status', controller.getScheduleStatus); // New endpoint
+router.get('/schedule-status', controller.getScheduleStatus);
 
 module.exports = router;
