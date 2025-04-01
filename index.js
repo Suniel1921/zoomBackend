ee88065b5bb7eb21372ac0d7d855dd2a1c5a9acc
sunil sharma <gitbysuniel@gmail.com>
2025-04-01 15:04:09 +0900

    fixed bugs

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ webSocketService.initialize(server);
 
 // Middleware for parsing JSON and URL-encoded data
 // app.use(express.json());
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // CORS Middleware
